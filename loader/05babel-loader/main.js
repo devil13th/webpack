0ea6ad2dcd670f34a543a2545ef674a9d823e8f7
十三妖 <181907667@qq.com>
2020-03-06 00:21:41 +0800

    babel

--- loader/05babel-loader/main.js
+++ loader/05babel-loader/main.js
@@ -4,10 +4,22 @@ import ReactDOM  from 'react-dom';
 import img1 from './images/big.png';
 import img2 from './images/small.png';
 
+
+
+import './style/app.css';
+import './style/b.css';
+import './style/c.less';
+import './style/iconfont.css';
+
+import MyComponent from './MyComponent.jsx';
+
+
+
 ReactDOM.render(
   <div>
     <img src={img1}/>
     <img src={img2}/>
+    <MyComponent name="hello"></MyComponent>
   </div>,
   document.getElementById('example')
 );
